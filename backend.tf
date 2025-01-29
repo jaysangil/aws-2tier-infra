7c18e36b5198a3bd0fc2149475f29ddf9c409ef4
takuya-sangil <jayloveswonyoung@gmail.com>
2025-01-30 01:00:29 +0800

    pre-staged files

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "aws-2tier-infra-devops"
     region         = "us-east-1"
-    key            = "DevOps Project-11/terraform.tfstate"
+    key            = "terraform/state/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
